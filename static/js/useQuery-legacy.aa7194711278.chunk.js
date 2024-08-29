7d2c4fc6a8eb0b4645072183db0d10e40aecd8de
linjxw <127081938+linjxw@users.noreply.github.com>
2024-08-29 20:36:41 +0800

    Add files via upload

---
+++ static/js/useQuery-legacy.aa7194711278.chunk.js
@@ -0,0 +1,1 @@
+System.register(["./vendor-legacy.39ec95964c16.chunk.js"],(function(e,r){"use strict";var t;return{setters:[e=>{t=e.o}],execute:function(){e("u",(()=>{const{search:e}=t();return new URLSearchParams(e)}))}}}));
