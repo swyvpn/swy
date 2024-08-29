5e986d4a3e3bea5e631db4fc6cfb99d0a160201d
swyvpn <duudcc@gmail.com>
2024-08-29 22:16:17 +0800

    Delete static directory

--- static/js/useQuery-legacy.aa7194711278.chunk.js
+++
@@ -1,1 +0,0 @@
-System.register(["./vendor-legacy.39ec95964c16.chunk.js"],(function(e,r){"use strict";var t;return{setters:[e=>{t=e.o}],execute:function(){e("u",(()=>{const{search:e}=t();return new URLSearchParams(e)}))}}}));
